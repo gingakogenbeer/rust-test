5d6e1b8a9dbe179f181678e7ca3d2ab2ed251c89
Gingakogen Beer <year0120@gmail.com>
2020-03-30 15:09:23 +0900

    upload

--- Euclidean Algorithm2.rs
+++ Euclidean Algorithm2.rs
@@ -11,6 +11,11 @@ fn gcd(mut x:u64, mut y:u64) -> u64{
     }
     x
 }
+#[test]
+fn test_gcd(){
+    assert_eq!(gcd(14, 15), 1);
+    assert_eq!(gcd(2*3*5*11*17, 3*7*11*13*19), 3*11);
+}
 fn main(){
     println!("Please input two numbers.");
     let mut num1 = String::new();
